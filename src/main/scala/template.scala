fd936e88fe5190c1c7a9aaa6ec45a7e317a6ad4c
atichat lappanopakon <atichat.lap@gmail.com>
2020-12-11 16:36:08 +0700

    Update template

--- src/main/scala/template.scala
+++ src/main/scala/template.scala
@@ -1,5 +1,7 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
 
 object TemplateSpark {
 
